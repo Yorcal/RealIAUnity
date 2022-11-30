43c08c641e709bab9390f63f8bb24fc628ea0b05
Yorcal <43748206+Yorcal@users.noreply.github.com>
2022-11-30 16:22:28 +0100

    player movement demo

---
+++ VraiIA/Assets/Scripts/Pathfinding/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GridPathFinding grid;
+    public Vector3 nextPosistion = Vector3.zero;
+    public float speed = 5f;
+    public float turnSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = GameObject.Find("A*").GetComponent<GridPathFinding>();
+        nextPosistion = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(grid.path.Count>0){
+            nextPosistion = grid.GetSeekerNextPosition();
+        }
+        
+        if (nextPosistion != transform.position){
+            transform.position = Vector3.MoveTowards(transform.position, nextPosistion, speed * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(nextPosistion - transform.position), turnSpeed * Time.deltaTime);
+        }
+    }
+}
